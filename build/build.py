7cf92726e10fb28fbb16794586a0243f35086b30
Wyatt Ho <Wyatt@xing.com>
2023-07-06 15:11:12 +0800

    feat: get_md_names

--- build/build.py
+++ build/build.py
@@ -1,4 +1,6 @@
 import configparser
+import os
+
 from PyHackMD import API
 
 
@@ -9,5 +11,22 @@ def get_hachmd_token() -> str:
     return config['hackmd']['token']
 
 
-api = API(get_hachmd_token())
-data = api.get_note_list()
+def get_md_names() -> list:
+    SRC_PATH = '.\\src'
+    MD_EXT = 'md'
+    md_names = [
+        os.path.join(SRC_PATH, path)
+        for path in os.listdir(SRC_PATH) if path.endswith(f'.{MD_EXT}')
+    ]
+    return md_names
+
+
+def main() -> None:
+    # api = API(get_hachmd_token())
+    # data = api.get_note_list()
+
+    md_names = get_md_names()
+
+
+if __name__ == '__main__':
+    main()
