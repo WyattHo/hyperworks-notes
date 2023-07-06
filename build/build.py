88308a7226cac1451940f9e03257af4034efde8a
Wyatt Ho <Wyatt@xing.com>
2023-07-06 16:53:29 +0800

    feat: modified the format of the return from link_repo_imgs

--- build/build.py
+++ build/build.py
@@ -24,28 +24,29 @@ def link_repo_imgs(
         url_suf: str = '?raw=true') -> None:
 
     with open(md_path_ori, 'r') as f:
-        conts_ori = f.readlines()
+        lines_ori = f.readlines()
 
-    conts_new = []
-    for cont_ori in conts_ori:
-        if '<img src' in cont_ori:
-            cont_new = cont_ori.replace('../', repo_url)
-            cont_new = cont_new.replace('.png', f'.png{url_suf}')
+    lines_mod = []
+    for line_ori in lines_ori:
+        if '<img src' in line_ori:
+            line_mod = line_ori.replace('../', repo_url)
+            line_mod = line_mod.replace('.png', f'.png{url_suf}')
+            lines_mod.append(line_mod)
         else:
-            cont_new = cont_ori
-        conts_new.append(cont_new)
-
-    md_path_new = md_path_ori.replace('src', 'build')
-    with open(md_path_new, 'w') as f:
-        f.writelines(conts_new)
+            lines_mod.append(line_ori)
+    
+    title = os.path.basename(md_path_ori).capitalize().rstrip('.md')
+    content = ''.join(lines_mod)
+    return {title: content}
 
 
 def main() -> None:
-    # api = API(get_hachmd_token())
-    # data = api.get_note_list()
-
     md_paths = get_md_paths()
-    list(map(link_repo_imgs, md_paths))  # Didn't work if no list()
+    for path in md_paths:
+        md_content = link_repo_imgs(path)
+
+    # api = API(get_hachmd_token())
+    # notes = api.get_note_list()
 
 
 if __name__ == '__main__':
